d2286aff4e617ac96021c0f62d731f83e3598722
DaanyaalBawla <daanyaal.b231@gmail.com>
2025-09-20 15:41:26 -0400

    started to add parts for criteria function

--- models/courses.js
+++ models/courses.js
@@ -2,11 +2,13 @@ const { readFile } = require('fs')
 const fsProm = require('fs.promises')
 const path = require('path')
 class Courses {
-    async find(criteria) {
-        courseRead = await fsProm.readFile('coursePath');
-
-    }
     coursePath = path.join(__dirname,'courses.json')
+    async find(criteria) {
+       function criteria(c) = 
+        const courseRead = await fsProm.readFile('coursePath');
+        const parseCourse = JSON.parse(courseRead)
+        const filteredArr = parseCourse.filter(criteria)
+    }
     
 } 
 module.exports = new Courses
