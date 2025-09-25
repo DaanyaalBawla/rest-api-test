7d7f98f5cc200fd1c775bd593517cad5f41dd5d1
DaanyaalBawla <daanyaal.b231@gmail.com>
2025-09-25 00:05:25 -0400

    started courses.js in services

---
+++ services/courses.js
@@ -0,0 +1,9 @@
+class CourseServices {
+    getCoursesBySubject(subject) {
+        return subject
+    }
+    getCoursesByGenEdCategory(catagory) {
+        return catagory
+    }
+}
+module.exports = new CourseServices
