b8440a22319323b9e4a37d9c9799f0c3fac57f74
DaanyaalBawla <daanyaal.b231@gmail.com>
2025-11-18 18:09:08 -0500

    menu.js with get request

---
+++ routes/menu.js
@@ -0,0 +1,17 @@
+const hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi')
+const server = hapi.server({
+            port: 3001,
+            host: 'localhost'
+        });
+module.exports = [
+    server.route({
+        method: 'GET',
+        path: '/menu/{menu_item}',
+        handler: function (request, h) {
+        return `${request.params.menu_item}`;
+        }
+
+    })
+
+]
