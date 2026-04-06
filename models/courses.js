0aef549313480b805ea7ab45fde7dafd1af545c0
DaanyaalBawla <110138202+DaanyaalBawla@users.noreply.github.com>
2026-04-06 00:05:52 -0400

    fixed to use promises and fixed refernce error related to it

--- models/courses.js
+++ models/courses.js
@@ -1,12 +1,12 @@
 const { readFile } = require('node:fs');
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 class Courses {
     coursePath = path.join(__dirname,'courses.json')
     async find(criteria) {
          (criteria) => {return true}
         
-        const courseRead = await fsProm.readFile(this.coursePath);
+        const courseRead = await fs.readFile(this.coursePath);
         const parseCourse = JSON.parse(courseRead)
         const filteredArr = parseCourse.filter(criteria)
     }
