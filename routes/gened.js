82171150f2058997b7a03f112b566b8d2e04b201
DaanyaalBawla <daanyaal.b231@gmail.com>
2026-05-13 09:22:34 -0400

    Update gened.js

--- routes/gened.js
+++ routes/gened.js
@@ -1,17 +1,12 @@
-const hapi = require('@hapi/hapi');
 const Joi = require('@hapi/joi')
-const server = hapi.server({
-            port: 3001,
-            host: 'localhost'
-        });
 module.exports = [
-    server.route({
+    {
         method: 'GET',
         path: '/gened/{gened_category_code}',
         handler: function (request, h) {
         return `${request.params.gened_category_code}`;
         }
 
-    })
+    }
 
 ]
